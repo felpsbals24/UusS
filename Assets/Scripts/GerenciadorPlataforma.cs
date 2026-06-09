4de7265835f726bc1eefc5531356d3b514fa887d
KaduFF974 <carloseduardoff974@gmail.com>
2026-06-08 23:10:12 -0300

    Atualização Versão Mobile
    Botões corrigidos e resolução também.

---
+++ Assets/Scripts/GerenciadorPlataforma.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GerenciadorPlataforma : MonoBehaviour
+{
+    [Header("Objeto com todos os botões mobile")]
+    public GameObject painelControlesMobile;
+
+    void Awake()
+    {
+        // Verifica a plataforma do jogo
+#if UNITY_ANDROID || UNITY_IOS
+        // Se for celular, ativa os botões na tela
+        if (painelControlesMobile != null) painelControlesMobile.SetActive(true);
+#else
+            // Se for PC (Windows/Mac) ou o próprio Editor da Unity, esconde os botões
+            if (painelControlesMobile != null) painelControlesMobile.SetActive(false);
+#endif
+    }
+}
